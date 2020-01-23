017dcf89d9ff9a2ca66a04542dee7f485265f852
Sergey ILIN <ilineserg@gmail.com>
2020-01-23 19:44:28 +0300

    add tests

--- robot.py
+++ robot.py
@@ -1,5 +1,11 @@
+import argparse
+import datetime
+import json
+import sys
+import random
 from collections import namedtuple
 from enum import Enum
+from time import sleep, time
 
 
 class Colors(Enum):
@@ -20,6 +26,27 @@ class RotateDirection(Enum):
     BACKWARD = 0
     LEFT = 1
     RIGHT = -1
+
+
+class Commands(Enum):
+    KEY_UP = 1
+    KEY_DOWN = 2
+    KEY_LEFT = 3
+    KEY_RIGHT = 4
+    ROTATE_LEFT = 5
+    ROTATE_RIGHT = 6
+    REVERSE = 7
+
+
+KEY_COMMANDS = {
+    "W": Commands.KEY_UP,
+    "S": Commands.KEY_DOWN,
+    "D": Commands.KEY_RIGHT,
+    "A": Commands.KEY_LEFT,
+    "REVERSE": Commands.REVERSE,
+    "LEFT": Commands.ROTATE_LEFT,
+    "RIGHT": Commands.ROTATE_RIGHT,
+}
 
 
 class Robot:
@@ -68,28 +95,33 @@ class Game:
     WALL_SIGN = '#'
     FREE_SIGN = '-'
 
-    is_live = False
-
-    def __init__(self, height, width, robot):
+    def __init__(self, height, width, robot, field=None, obstacles=None):
         self.robot = robot
         self.width = width
         self.height = height
-
-        self.commands = []
-        self.field = ['-'] * self.height * self.width
         self.center = self.height // 2 * self.width + self.width // 2
         self.last_command = None
-        self.set_wall()
+        self.error_message = ''
+        self.commands = []
+
+        self.obstacles_count = obstacles if obstacles else 10
+
+        if field is None:
+            self.field = ['-'] * self.height * self.width
+            self.set_wall()
+            self.set_obstacles()
+        else:
+            self.field = field
 
     def normalize_position(self, position: Point) -> int:
         return self.center + position.x - self.width * position.y
 
     def get_robot_body_idx(self) -> list:
-        normalized_position = self.normalize_position(self.robot.get_position())
-        row_up = normalized_position - self.width
-        row_bottom = normalized_position + self.width
+        position = self.normalize_position(self.robot.get_position())
+        row_up = position - self.width
+        row_bottom = position + self.width
         robot_coord = [row_up - 1, row_up, row_up + 1,
-                       normalized_position - 1, normalized_position, normalized_position + 1,
+                       position - 1, position, position + 1,
                        row_bottom - 1, row_bottom, row_bottom + 1]
         return robot_coord
 
@@ -100,13 +132,39 @@ class Game:
                  if robot_direction != new_direction else None)
         return RotateDirection(cross)
 
+    def get_area_indexes(self, center, size):
+        centers = [center]
+        for i in range(1, size // 2 + 1):
+            centers.append(center - self.width * i)
+            centers.append(center + self.width * i)
+
+        area = []
+        for i in centers:
+            area.extend(list(range(i - size // 2, i + size // 2 + 1)))
+        return area
+
     def set_wall(self):
         for idx, _ in enumerate(self.field):
             if (idx < self.width
                     or idx % self.width == 0
                     or (idx + 1) % self.width == 0
                     or idx > self.width * self.height - self.width):
-                self.field[idx] = colorize(self.WALL_SIGN, Colors.RED)
+                self.field[idx] = self.WALL_SIGN
+
+    def set_obstacles(self):
+        position = self.normalize_position(self.robot.get_position())
+        safe_area = self.get_area_indexes(position, 9)
+
+        count = self.obstacles_count
+        while count > 0:
+            position = random.randint(0, self.height * self.width - 1)
+            if position not in safe_area:
+                area = self.get_area_indexes(position, random.choice([1, 2, 3, 4]))
+                for idx in area:
+                    if (0 <= idx < self.width * self.height
+                            and idx not in safe_area):
+                        self.field[idx] = self.WALL_SIGN
+                count -= 1
 
     def render(self):
         # clear screen
@@ -122,7 +180,7 @@ class Game:
                 item = colorize(robot_body[robot_render_idx], self.robot.color)
                 robot_render_idx += 1
             else:
-                item = fld
+                item = colorize(fld, Colors.RED) if fld == self.WALL_SIGN else fld
 
             if (idx + 1) % self.width == 0:
                 print(item)
@@ -130,26 +188,32 @@ class Game:
                 print(item, end=" ")
 
         # render info
-        print(f'Робот повернут: {self.robot.get_direction()}')
+        print(colorize(f'Robot direction: {self.robot.get_direction()}', Colors.GREEN))
         position = self.normalize_position(self.robot.get_position())
-        print(f'Вы сходили: {self.last_command or ""}, {position}')
+        position_coord = self.robot.get_position()
+        print(colorize(f'Your command: {self.last_command or ""}', Colors.GREEN))
+        print(colorize(f'Your step: {position}', Colors.GREEN))
+        print(colorize(f'Your step coordinate: {position_coord}', Colors.GREEN))
 
-        if self.is_live:
-            print('\u2191 - move up, \u2193 - move down, \u2192 - move right, \u2190 - move left')
+        if self.error_message:
+            print(colorize(f"Error Message: {self.error_message}", Colors.RED))
+            self.error_message = ''
         else:
-            print('W - move up, S - move down, D - move right, A - move left')
-            print('REVERSE - rotate 180, LEFT - left rotate 90, RIGHT - right rotate 90')
+            print()
+
+        print('W - move up, S - move down, D - move right, A - move left')
+        print('REVERSE - rotate 180, LEFT - left rotate 90, RIGHT - right rotate 90')
 
     def can_move(self):
-        front_center = self.normalize_position(Point(*map(sum, zip(self.robot.position, self.robot.direction))))
-        if self.robot.direction.x != 0:
-            front_idx = [front_center - 1,
-                         front_center,
-                         front_center + 1]
+        one_step = Point(*map(sum, zip(self.robot.get_position(),
+                                       self.robot.get_direction())))
+        center = self.normalize_position(
+            Point(*map(sum, zip(one_step, self.robot.get_direction()))))
+
+        if self.robot.direction.x == 0:
+            front_idx = [center - 1, center, center + 1]
         else:
-            front_idx = [front_center - self.width,
-                         front_center,
-                         front_center + self.width]
+            front_idx = [center - self.width, center, center + self.width]
         for idx in front_idx:
             if self.field[idx] != '-':
                 return False
@@ -157,47 +221,93 @@ class Game:
 
     def update(self, command: str):
         command = command.upper()
+        cmd = KEY_COMMANDS.get(command)
         direction = self.robot.get_direction()
-        if command == "W":
+
+        if cmd == Commands.KEY_UP:
             direction = Point(0, 1)
-        elif command == "S":
+        elif cmd == Commands.KEY_DOWN:
             direction = Point(0, -1)
-        elif command == "D":
+        elif cmd == Commands.KEY_RIGHT:
             direction = Point(1, 0)
-        elif command == "A":
+        elif cmd == Commands.KEY_LEFT:
             direction = Point(-1, 0)
 
-        if command == "REVERSE":
+        if cmd == Commands.REVERSE:
             self.robot.rotate(RotateDirection.BACKWARD)
-        elif command == "LEFT":
+        elif cmd == Commands.ROTATE_LEFT:
             self.robot.rotate(RotateDirection.LEFT)
-        elif command == "RIGHT":
+        elif cmd == Commands.ROTATE_RIGHT:
             self.robot.rotate(RotateDirection.RIGHT)
-        elif command == "LIVE":
-            self.is_live = True
-        elif command == "ESC":
-            self.is_live = False
         else:
-            if direction != self.robot.get_direction():
-                self.robot.rotate(self.get_rotate_direction(direction))
+            if cmd is not None:
+                if direction != self.robot.get_direction():
+                    self.robot.rotate(self.get_rotate_direction(direction))
+                else:
+                    if self.can_move():
+                        self.robot.move()
+                    else:
+                        self.error_message = "Can't move in that direction"
             else:
-                if self.can_move():
-                    self.robot.move()
+                self.error_message = f"\"{command}\" not supported!"
 
         self.commands.append(command)
         self.last_command = self.commands[-1]
         self.render()
 
 
-def run_game():
+def run_game(height, width):
     robot = Robot()
-    game = Game(20, 30, robot)
+    game = Game(height, width, robot, obstacles=0)
     game.render()
-    while True:
-        if not game.is_live:
+    try:
+        while True:
             command = input('Ваш ход: ')
             game.update(command)
+    except KeyboardInterrupt:
+        save_game(game)
+        sys.exit(0)
+
+
+def run_replay(replay):
+    robot = Robot()
+    game = Game(replay['height'], replay['width'], robot, replay['field'])
+    game.render()
+    try:
+        for command in replay['commands']:
+            game.update(command)
+            sleep(1)
+    except KeyboardInterrupt:
+        sys.exit(0)
+
+
+def save_game(game: Game):
+    data = {
+        "field": game.field,
+        "height": game.height,
+        "width": game.width,
+        "commands": game.commands
+    }
+    date = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    with open(f"replay_{date}.json", "w") as output:
+        json.dump(data, output)
 
 
 if __name__ == '__main__':
-    run_game()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--width", type=int, default=30,
+                        help="width of game field")
+    parser.add_argument("--height", type=int, default=20,
+                        help="height of game field")
+    parser.add_argument("--obstacles", type=int, default=10,
+                        help="number of obstacles on the field")
+    parser.add_argument("--replay", type=argparse.FileType('r'),
+                        help="number of obstacles on the field")
+
+    args = parser.parse_args()
+
+    if args.replay is None:
+        run_game(args.height, args.width)
+    else:
+        replay = json.load(args.replay)
+        run_replay(replay)
---
+++ tests.py
@@ -0,0 +1,105 @@
+import unittest
+from robot import *
+
+
+class TestRobot(unittest.TestCase):
+
+    def test_robot_getters(self):
+        robot = Robot()
+
+        self.assertEqual(robot.get_position(), Point(0, 0))
+
+        self.assertEqual(robot.get_direction(), Point(0, 1))
+
+        self.assertEqual(robot.get_body(), robot.rotated_up)
+        robot.direction = Point(0, -1)
+        self.assertEqual(robot.get_body(), robot.rotated_down)
+        robot.direction = Point(-1, 0)
+        self.assertEqual(robot.get_body(), robot.rotated_left)
+        robot.direction = Point(1, 0)
+        self.assertEqual(robot.get_body(), robot.rotated_right)
+
+    def test_robot_actions(self):
+        robot = Robot()
+
+        self.assertEqual(robot.position, Point(x=0, y=0))
+        self.assertEqual(robot.direction, Point(x=0, y=1))
+        robot.move()
+        self.assertEqual(robot.position, Point(x=0, y=1))
+        self.assertEqual(robot.direction, Point(x=0, y=1))
+        robot.rotate(RotateDirection.LEFT)
+        self.assertEqual(robot.position, Point(x=0, y=1))
+        self.assertEqual(robot.direction, Point(x=-1, y=0))
+        robot.move()
+        self.assertEqual(robot.position, Point(x=-1, y=1))
+        self.assertEqual(robot.direction, Point(x=-1, y=0))
+        robot.rotate(RotateDirection.RIGHT)
+        self.assertEqual(robot.position, Point(x=-1, y=1))
+        self.assertEqual(robot.direction, Point(x=0, y=1))
+        robot.move()
+        self.assertEqual(robot.position, Point(x=-1, y=2))
+        self.assertEqual(robot.direction, Point(x=0, y=1))
+        robot.rotate(RotateDirection.BACKWARD)
+        self.assertEqual(robot.position, Point(x=-1, y=2))
+        self.assertEqual(robot.direction, Point(x=0, y=-1))
+        robot.move()
+        self.assertEqual(robot.position, Point(x=-1, y=1))
+        self.assertEqual(robot.direction, Point(x=0, y=-1))
+
+
+class TestGame(unittest.TestCase):
+
+    def test_normalize_position(self):
+        robot = Robot()
+        game = Game(30, 30, robot)
+
+        self.assertEqual(game.normalize_position(Point(x=0, y=0)), 465)
+        self.assertEqual(game.normalize_position(Point(x=0, y=13)), 75)
+        self.assertEqual(game.normalize_position(Point(x=7, y=13)), 82)
+        self.assertEqual(game.normalize_position(Point(x=3, y=6)), 288)
+        self.assertEqual(game.normalize_position(Point(x=-6, y=-3)), 549)
+
+    def test_get_robot_body_idx(self):
+        robot = Robot()
+        game = Game(30, 30, robot)
+
+        self.assertEqual(game.get_robot_body_idx(),
+                         [434, 435, 436, 464, 465, 466, 494, 495, 496])
+        game.robot.position = Point(x=7, y=13)
+        self.assertEqual(game.get_robot_body_idx(),
+                         [51, 52, 53, 81, 82, 83, 111, 112, 113])
+
+    def test_get_area_indexes(self):
+        robot = Robot()
+        game = Game(30, 30, robot)
+        position_1 = game.normalize_position(Point(x=0, y=0))
+        position_2 = game.normalize_position(Point(x=7, y=13))
+        position_3 = game.normalize_position(Point(x=-1, y=-5))
+        area_1 = [464, 465, 466, 434, 435, 436, 494, 495, 496]
+        area_2 = [80, 81, 82, 83, 84, 50, 51, 52, 53, 54, 110, 111, 112, 113,
+                  114, 20, 21, 22, 23, 24, 140, 141, 142, 143, 144]
+        area_3 = [613, 614, 615, 583, 584, 585, 643, 644, 645]
+        self.assertEqual(game.get_area_indexes(position_1, 3), area_1)
+        self.assertEqual(game.get_area_indexes(position_2, 4), area_2)
+        self.assertEqual(game.get_area_indexes(position_3, 2), area_3)
+
+    def test_can_move(self):
+        robot = Robot()
+        game = Game(30, 30, robot)
+        game.robot.position = Point(x=0, y=13)
+        game.robot.direction = Point(x=0, y=1)
+
+        self.assertEqual(game.can_move(), False)
+
+        game.robot.direction = Point(x=0, y=-1)
+        self.assertEqual(game.can_move(), True)
+
+        game.robot.direction = Point(x=1, y=0)
+        self.assertEqual(game.can_move(), True)
+
+        game.robot.direction = Point(x=-1, y=0)
+        self.assertEqual(game.can_move(), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
